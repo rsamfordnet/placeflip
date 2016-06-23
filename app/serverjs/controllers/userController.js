776230cf63c72be2373ad0d35cc503d3fa6de66f
Ryan Samford <rsamfordnet@gmail.com>
2016-06-23 01:16:22 -0700

    Image upload through cloudinary

--- app/serverjs/controllers/userController.js
+++ app/serverjs/controllers/userController.js
@@ -1,3 +1,15 @@
+var Busboy = require('busboy');
+var fs	 = require("fs");
+var http = require('http');
+var util = require('util');
+var cloudinary = require("cloudinary");
+
+cloudinary.config({ 
+  cloud_name: 'hhrfxfead', 
+  api_key: '256916526976278', 
+  api_secret: 'dYmCplLTK0iBsb-9b2sQV3zgad4' 
+});
+
 module.exports = {
 	setup : function(app, repository, requireSession, userSockets)
 	{
@@ -43,6 +55,93 @@ module.exports = {
 						return res.sendStatus(200);
 					}
 				);
+			}
+		);
+
+		app.get("/pf/picture", 
+			function(req, res){
+				return res.render("picture");
+			}
+		);
+
+		app.post("/pf/picture",
+			function(req, res)
+			{
+				var busboy = new Busboy({ headers: req.headers });
+				var stream = cloudinary.uploader.upload_stream(function(result) { console.log("FROM CLOUDINARY : " + util.inspect(result)); }, 
+					{
+						public_id : 'sample_id', 
+						crop	  : 'limit',
+						width     : 2000,
+						height    : 2000,
+						eager     : [
+							{ 
+								width: 200, 
+								height: 200, 
+								crop: 'thumb', 
+								gravity: 'face',
+								radius: 20, 
+								effect: 'sepia' 
+							},
+							{ 
+								width: 100, 
+								height: 150, 
+								crop: 'fit', 
+								format: 'jpg' 
+							}
+						],                   
+						tags      : ['profile']
+					}      
+  				);
+
+				busboy.on(
+					'file', 
+					function(fieldname, file, filename, encoding, mimetype) 
+					{
+						console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+
+						file.on(
+							'data', 
+							function(data) 
+							{
+								console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+
+								console.log('Writing to stream.');
+								stream.write(data);
+							}
+						);
+
+						file.on(
+							'end', 
+							function() 
+							{
+								console.log('File [' + fieldname + '] Finished');
+								stream.end();
+							}
+						);
+					}
+				);
+
+				busboy.on(
+					'field', 
+					function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) 
+					{
+						console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+					}
+				);
+
+				busboy.on(
+					'finish', 
+					function() 
+					{
+						console.log('Done parsing form!');
+						res.writeHead(303, { Connection: 'close', Location: '/' });
+						res.end();
+					}
+				);
+
+
+				req.pipe(busboy);
 			}
 		);
 
